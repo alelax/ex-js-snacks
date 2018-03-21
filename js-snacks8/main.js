c974aafd7a1fcb50ef5aca1d27055ce9d9cb71c0
alelax <alessandro.lausdei@gmail.com>
2018-03-21 17:54:43 +0100

    js-snacks - 8 Updated

--- js-snacks8/main.js
+++ js-snacks8/main.js
@@ -12,7 +12,7 @@ nel caso la tab fosse attiva, deve attivare la successiva
 
 */
 
-var w_init = {
+var w = {
    "tab" : [{"name" : "facebook", "type" : "social" },
             {"name" : "twitter", "type" : "social" },
             {"name" : "CorriereDellaSera", "type" : "news" },
@@ -20,19 +20,47 @@ var w_init = {
             {"name" : "Gmail", "type" : "communication" },
             {"name" : "Airbnb", "type" : "trip" }],
 
-   "active_tab" : [0, 1, 0, 0, 0, 0]
+   "active_tab" : [1, 0, 0, 0, 0, 0]
 };
 
 
 for (var i = 0; i < w.tab.length; i++) {
-   if(w.active_tab[i]) {
-      if ( w.tab[i].type == "social" ) {
-         w.tab.splice(i, 1);
-         w.active_tab.splice(i,1);
-         w.active_tab[i] = 1;
+   if ( w.tab[i].type == "social" ) {
+      w.tab[i] = null;
+      if(w.active_tab[i]) {
+         w.active_tab[i+1] = 1;
       }
-
+      w.active_tab[i] = null;
    }
+}
+console.log(refresh(w.tab));
+console.log(refresh(w.active_tab));
+
+
+
+
+/*
+   for (var i = 0; i < w.tab.length; i++) {
+      if(w.active_tab[i]) {
+         if ( w.tab[i].type == "social" ) {
+            w.tab[i] = null;
+            w.active_tab[i] = null;
+            w.active_tab[i+1] = 1;
+         }
+      }
+   }
+   console.log(refresh(w.tab));
+   console.log(refresh(w.active_tab));
+*/
+
+// Funzione che riceve un array, controlla se al suo interno sono presenti valori
+// nulli e nel caso li elimina.
+function refresh(arr) {
+   while( arr.includes(null) ) {
+      var index = arr.indexOf(null);
+      arr.splice(index,1);
+   }
+   return arr;
 }
 
 console.log("After: ");

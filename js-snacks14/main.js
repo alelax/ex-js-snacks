9cdf51a51a2cda7124118a29cd5537b429b1794a
alelax <alessandro.lausdei@gmail.com>
2018-03-23 11:32:19 +0100

    js-snacks14

--- js-snacks14/main.js
+++ js-snacks14/main.js
@@ -1,43 +1,22 @@
-var num = [ randomNumber(1,100),
-            randomNumber(1,100),
-            randomNumber(1,100),
-            randomNumber(1,100),
-            randomNumber(1,100)
-          ];
+/*
+   Simulare un countdown di 10 secondi che alla fine dice buon anno
+*/
 
-alert(num);
+var Initialseconds = 10;
 
-window.setTimeout( function(){
+var v = window.setInterval(function(){
 
-      var listNum = new Array();
-      var result = new Array();
-
-      // Il ciclo da la possibilità all'utente di inserire i numeri, che verranno
-      //poi salvati in un array
-      for (var i = 0; i < 5; i++) {
-         usrNum = parseInt( prompt("inserisci un numero: ") );
-         listNum.push(usrNum);
+      console.log(Initialseconds);
+      Initialseconds--;
+      if(Initialseconds == -1) {
+         alert("buon anno!");
+         stopCountdown(v);
       }
 
-      result = chekOrderNumber(num, listNum);
-      alert("Hai indovintato " + result.length + " numeri: " + result);
-
-   }, 2000);
+}, 1000, Initialseconds)
 
 
-// Funzione che stabilisce quali e quanti numeri inseriti dall'utente sono stati indovinati
-function chekOrderNumber(startList, userList) {
-   var result = new Array();
-   for (var i = 0; i < num.length; i++) {
-      if (startList[i]==userList[i]) {
-         result.push(startList[i]);
-      }
-   }
-   return result;
-}
 
-//Funzione che genera un numero casuale compreso tra l'estremo inferiore start
-//e l'estremo superiore end, inclusi.
-function randomNumber(start, end) {
-   return Math.floor(Math.random() * (end - start + 1)) + start;
+function stopCountdown(stop) {
+   window.clearInterval(stop);
 }
